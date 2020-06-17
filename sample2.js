48d7901f95a4104036c91482739b7755999f3a38
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2020-06-16 21:55:55 -0400

    Update sample2.js

--- sample2.js
+++ sample2.js
@@ -6,19 +6,23 @@ osc( 215, 0.1, 2 )
   osc( 2, -0.3, 100 )
   .rotate(15)
 )
+// provides the red 'northern light' effect within the discs
 .mult(
   osc( 215, -0.1, 2)
   .pixelate( 50, 50 )
 )
+// even out the 'nothern light' effect
 .color( 0.9, 0.0, 0.9 )
 .modulate(
   osc( 6, -0.1 )
   .rotate( 9 )
 )
+// commenting out this function provides a cool directional effect to the northern lights
 .add(
   osc( 10, -0.9, 900 )
   .color(1,0,1)
 )
+// adds the floating 'discs'
 .mult(
   shape(90, 0.2, 1)
   .luma()
@@ -28,9 +32,10 @@ osc( 215, 0.1, 2 )
 )
 // wave
 .modulate(
-  osc( 9, -0.3, 900 )
-  .rotate( 1 )
+  osc( 12, -0.3, 900 )
+  .rotate( 6 )
 )
+// adds a gradient background shift
 .add(
   osc(4, 1, 90)
   .color(0.2,0,1)
