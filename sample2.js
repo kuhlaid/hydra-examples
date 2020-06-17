6e3dd6c1a37d9f38f5a172e1aff8a293a7115034
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2020-06-16 21:39:47 -0400

    Create sample2.js

---
+++ sample2.js
@@ -0,0 +1,38 @@
+// by Zach Krall
+// http://zachkrall.online/
+
+osc( 215, 0.1, 2 )
+.modulate(
+  osc( 2, -0.3, 100 )
+  .rotate(15)
+)
+.mult(
+  osc( 215, -0.1, 2)
+  .pixelate( 50, 50 )
+)
+.color( 0.9, 0.0, 0.9 )
+.modulate(
+  osc( 6, -0.1 )
+  .rotate( 9 )
+)
+.add(
+  osc( 10, -0.9, 900 )
+  .color(1,0,1)
+)
+.mult(
+  shape(90, 0.2, 1)
+  .luma()
+  .repeatX(2)
+  .repeatY(2)
+  .colorama(10)
+)
+// wave
+.modulate(
+  osc( 9, -0.3, 900 )
+  .rotate( 1 )
+)
+.add(
+  osc(4, 1, 90)
+  .color(0.2,0,1)
+)
+.out()
